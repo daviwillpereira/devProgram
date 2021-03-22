a872b8d86cc72079711fa45786d72c8e59075063
Davi Pereira <daviwillpereira@gmail.com>
2021-03-22 16:14:32 -0300

    first commit

---
+++ src/main/java/com/devProgram/javaCoreAssessment/JavaCoreAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.devProgram.javaCoreAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoreAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devProgram/javaCoreAssessment/JavaCoreAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devProgram.javaCoreAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoreAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
